fc1b48a5392f4d8ccf8b3a9ee3d948494317c458
HesamMirzaie <mirzaeeh08@gmail.com>
2023-11-01 00:10:07 +0330

    add project

---
+++ src/components/Counter.tsx
@@ -0,0 +1,26 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from '../state/store';
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+} from '../state/counter/counterSlice';
+
+const Counter = () => {
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h2>{count}</h2>
+      <div>
+        <button onClick={() => dispatch(incrementByAmount(10))}>
+          Increment
+        </button>
+        <button onClick={() => dispatch(decrement())}>Decrement</button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
